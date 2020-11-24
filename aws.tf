e7211667cf1436cda79931c9b3124421acb2c495
Julius Rajala <juliusrajala@gmail.com>
2020-11-24 17:06:26 +0200

    Adds comments for provider selection.

--- aws.tf
+++ aws.tf
@@ -7,11 +7,13 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
+# Our default AWS provider is set up just next door of Finland in Stockholm
 provider "aws" {
   region = "eu-north-1"
 }
 
+# We'll need a secondary provider though, this one will be used for a couple of services
+# only available in the US.
 provider "aws" {
   region = "us-east-1"
   alias  = "aws-us"
