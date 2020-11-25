f84f8067675d1add950172f59a63c9f6dfbb0cf1
Julius Rajala <juliusrajala@gmail.com>
2020-11-25 15:54:46 +0200

    Includes sane defaults for s3.

---
+++ modules/static-website/variables.tf
@@ -0,0 +1,29 @@
+variable "domain_name" {
+  description = "Domain name is also used to specify the names of relevant module-resources."
+}
+
+# S3 variables
+variable "s3_index_file" {
+  default = "index.html"
+}
+
+variable "s3_acl" {
+  default = "public-read"
+}
+
+# S3 variables used in the cors_rule block
+variable "cors_headers" {
+  default = ["*"]
+}
+
+variable "cors_methods" {
+  default = ["GET", "HEAD"]
+}
+
+variable "cors_origins" {
+  default = ["*"]
+}
+
+variable "cors_max_age" {
+  default = 1200
+}
